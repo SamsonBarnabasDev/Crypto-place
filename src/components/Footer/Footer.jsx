51d3f96cbea9dfe10e61abd7524b6a0ab71ac036
SamsonBarnabasDev <samsonbarnabas2002@gmail.com>
2024-09-28 05:06:04 +0530

    run

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      Copyright @ 2024, Cryptoplace - All Right Reserved.
+    </div>
+  );
+};
+
+export default Footer;
